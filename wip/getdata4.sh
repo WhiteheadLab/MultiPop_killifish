e76d124ce9f6d5b9384277ae01c135c86ed0951b
Jane Jungeun Park <jajpark@ucdavis.edu>
2020-04-04 15:03:23 -0700

    adding variant calling

--- wip/getdata4.sh
+++ wip/getdata4.sh
@@ -2,16 +2,17 @@
 #SBATCH -D /home/jajpark/niehs/scripts/
 #SBATCH -o /home/jajpark/niehs/slurm-log/wget3-stout-%j.txt
 #SBATCH -e /home/jajpark/niehs/slurm-log/wget3-stderr-%j.txt
-#SBATCH -J wget3
+#SBATCH -J wget_data
+#SBATCH --mem=16000
+#SBATCH -p high
+#SBATCH -t 24:00:00 
+
 set -e
 set -u
 
-###get data###
-
 cd ~/niehs/Data/
 
-wget -r -nH -nc -np -R index.html* \
-"http://slims.bioinformatics.ucdavis.edu/Data/ybpei8bxrj/" 
+wget -r --user=P202SC18122707-01_20190124_yKaRAK --password=03OadE http://hwftp.novogene.com 
 
 
 
